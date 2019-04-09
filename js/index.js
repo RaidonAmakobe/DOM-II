99be8eedf6f06bb1f0290b654c9739467e8012ba
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-04-09 17:46:16 -0400

    Partial Task 2
    - mouseover
    -created form page to use remainding listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+//mouseover
+
+let navbar = document.querySelector('.nav')
+navbar.addEventListener('mouseover', (event) => {event.target.style.color = "blue";});
+
+let navbarleave = document.querySelector('.nav')
+navbarleave.addEventListener('mouseout', (event) => {event.target.style.color = "black";});
+//keydown
+
+let emailButton = document.querySelector('.btn')
+emailButton.addEventListener('click', (event) => {prompt('Please input your email adress');});
+
+let keydown = document.querySelector('.btn')
+keydown.addEventListener('keydown', (event) => {console.log('keydown:', event.target.value);});
+
+//wheel
+
+//let wheel = document.querySelector('.nav');
+//wheel.addEventListener('wheel', (event) => {event.target.})
+//drag / drop
+
+// load
+
+//focus
+
+//resize
+
+//scroll
+
+//select
+
+//dblclick
+
