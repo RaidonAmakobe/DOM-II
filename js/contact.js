0db4ae1624916b484d33abfa17e9678f40416864
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-04-09 20:24:12 -0400

    MVP

--- js/contact.js
+++ js/contact.js
@@ -1,2 +1,11 @@
-let submit = document.querySelector('Submit');
-submit.textContent = 'Nice to meet you!';
+let thisKeydown = document.querySelector('input.youbwoi')
+thisKeydown.addEventListener('keydown', function(event) {console.log('keydown:', event.key)});
+thisKeydown.addEventListener('mouseover', function(event) {event.target.style.backgroundcolor = "#FFEBCD";});
+thisKeydown.addEventListener('mouseout', function(event) {event.target.style.backgroundcolor = "#FFEBCD";});
+thisKeydown.addEventListener('keyup', function(event) {console.log('keydown:', event.key)});
+
+let navbar = document.querySelector('.nav')
+navbar.addEventListener('mouseover', (event) => {event.target.style.color = "#FFEBCD";});
+
+let navbarleave = document.querySelector('.nav')
+navbarleave.addEventListener('mouseout', (event) => {event.target.style.color = "black";});
--- js/index.js
+++ js/index.js
@@ -1,34 +1,11 @@
 // Your code goes here
-//mouseover
-
 let navbar = document.querySelector('.nav')
-navbar.addEventListener('mouseover', (event) => {event.target.style.color = "blue";});
+navbar.addEventListener('mouseover', (event) => {event.target.style.color = "#FFEBCD";});
 
 let navbarleave = document.querySelector('.nav')
 navbarleave.addEventListener('mouseout', (event) => {event.target.style.color = "black";});
-//keydown
 
-let emailButton = document.querySelector('.btn')
+let emailButton = document.querySelector('.content-pick')
 emailButton.addEventListener('click', (event) => {prompt('Please input your email adress');});
 
-let keydown = document.querySelector('.btn')
-keydown.addEventListener('keydown', (event) => {console.log('keydown:', event.target.value);});
-
-//wheel
-
-//let wheel = document.querySelector('.nav');
-//wheel.addEventListener('wheel', (event) => {event.target.})
-//drag / drop
-
-// load
-
-//focus
-
-//resize
-
-//scroll
-
-//select
-
-//dblclick
 
